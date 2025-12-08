d37580018fe94b540f50dd08d3647a328abff50f
905458735 <muhammadqodir.112001@gmail.com>
2025-12-08 23:55:35 +0500

    minifest fullscren

--- api/bot.py
+++ api/bot.py
@@ -1,27 +1,49 @@
 import asyncio
-from os import getenv
-
+import os
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, InputFile
 from aiogram.filters import Command
-from aiogram.types import Message
+from dotenv import load_dotenv
 
-TOKEN = getenv("8206191170:AAFZW9iN2CXSxGEJ-llWvWxPk2efRGUvwhU")
+load_dotenv()  # .env faylni yuklash
+
+TOKEN = os.getenv("TELEGRAM_TOKEN") or os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise Exception("❌ Token topilmadi. .env faylni tekshiring!")
 
 dp = Dispatcher()
 
 
-# Command handler
 @dp.message(Command("start"))
-async def command_start_handler(message: Message) -> None:
-    await message.answer("Hello! I'm a bot created with aiogram.")
+async def start_handler(message: Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Open App",
+                    web_app=WebAppInfo(url="https://proguzmir.vercel.app/")
+                )
+            ]
+        ]
+    )
+
+    photo_path = os.path.join(os.path.dirname(__file__), "image/key.png")
+
+    if os.path.exists(photo_path):
+        await message.answer_photo(
+            photo=InputFile(photo_path),
+            caption="Salom hammaga",
+            reply_markup=keyboard
+        )
+    else:
+        await message.answer("'Hi, @! This is TapCoins 👋' 'Tap on the coin and watch your balance grow.' 'How much is TapCoins worth?' No one knows, probably nothing. Got any friends? Get them in the game. That way you'll get even more coins together. TapCoins is what you want it to be. That's all you need to know.", reply_markup=keyboard)
 
 
-# Run the bot
-async def main() -> None:
+async def main():
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-          
