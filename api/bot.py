a809a4671b5d28a02754426864bc0c7e00a9f45c
905458735 <muhammadqodir.112001@gmail.com>
2025-12-08 14:02:48 +0500

    op

--- api/bot.py
+++ api/bot.py
@@ -1,25 +1,20 @@
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
 API_TOKEN = "8206191170:AAFZW9iN2CXSxGEJ-llWvWxPk2efRGUvwhU"
-
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# /start va /help komandasi
-@dp.message(commands=["start", "help"])
-async def send_welcome(message: types.Message):
+@dp.message(Command("start"))
+async def start_cmd(message: types.Message):
     await message.answer("Hi!\nI'm your friendly bot!")
 
-# Echo handler
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-# Botni ishga tushirish
 async def main():
     await dp.start_polling(bot)
 
