4abf4ff91ba1fc35c07179e9d284d7ca33c908b3
905458735 <muhammadqodir.112001@gmail.com>
2025-12-09 16:42:17 +0500

    webhook

--- api/bot.py
+++ api/bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, InputFile
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, FSInputFile
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
@@ -21,24 +21,44 @@ async def start_handler(message: Message):
         inline_keyboard=[
             [
                 InlineKeyboardButton(
-                    text="Open App",
+                    text="Open the App",
                     web_app=WebAppInfo(url="https://proguzmir.vercel.app/")
                 )
             ]
         ]
     )
 
-    photo_path = os.path.join(os.path.dirname(__file__), "image/key.png")
+    photo_path = os.path.join(os.path.dirname(__file__), "coin.png")
+
+    # Username dynamic
+    username = message.from_user.username
+    if username:
+        mention = f"@{username}"
+    else:
+        mention = message.from_user.first_name
+
+    caption = f"""Hi, {mention}! This is ProgUzmiR 👋
+
+    Tap on the coin and watch your balance grow.
+
+    How much is ProgUzmiR worth? No one knows, probably nothing.
+
+    Got any friends? Get them in the game. That way you'll get even more coins together.
+
+    ProgUzmiR is what you want it to be. That's all you need to know.
+    """
 
     if os.path.exists(photo_path):
         await message.answer_photo(
-            photo=InputFile(photo_path),
-            caption="Salom hammaga",
+            photo=FSInputFile(photo_path),
+            caption=caption,
             reply_markup=keyboard
         )
     else:
-        await message.answer("'Hi, @! This is TapCoins 👋' 'Tap on the coin and watch your balance grow.' 'How much is TapCoins worth?' No one knows, probably nothing. Got any friends? Get them in the game. That way you'll get even more coins together. TapCoins is what you want it to be. That's all you need to know.", reply_markup=keyboard)
-
+        await message.answer(
+            text="There was an error.\nWe apologize for the inconvenience!",
+            reply_markup=keyboard
+        )
 
 async def main():
     bot = Bot(token=TOKEN)
