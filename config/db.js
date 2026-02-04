9aaafa8c0e6cea02d8cf1721e3ad75fcf87d8475
905458735 <muhammadqodir.112001@gmail.com>
2026-02-04 15:02:30 +0500

    op

--- config/db.js
+++ config/db.js
@@ -5,10 +5,4 @@ const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default function handler(req, res) {
-  // Bu funksiya Vercel serverida ishlaydi va .env ni o'qiy oladi
-  res.status(200).json({
-    ton_address: process.env.MERCHANT_TON,
-    evm_address: process.env.MERCHANT_EVM,
-  });
-}
+export default supabase
