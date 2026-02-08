0849c6388b3785313325e41d5c7fb4caf55c991d
905458735 <muhammadqodir.112001@gmail.com>
2026-02-08 08:25:19 +0500

    op

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ function renderGame() {
         rek.addEventListener('click', () => {
             if (isClaimedToday()) { showToast('Wait for the time to expire.'); return; }
 
-            const currentUrl = (location.protocol === window.location.origin + '/image/background1.jpg');
+            const currentUrl = (location.protocol === 'file:' ? 'https://proguzmir.vercel.app/image/background1.jpg' : window.location.origin + '/image/background1.jpg');
             const args = {
                 link: currentUrl,
                 text: 'I have successfully withdrawn 0.01 TON from ProgUzmiR, you can also play!',
